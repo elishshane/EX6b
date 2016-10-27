b5a6921f120fdf27c296f9001ca7679d56562da3
Shane Park <sp3558@nyu.edu>
2016-10-27 14:02:34 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,21 @@
+var commoncolor = color()
+
+function setup() {
+  createCanvas( 600, 600 );
+  background(175);
+  
+}
+
+function draw() {
+  fill( 255, 0, 0);
+  rect(125, 400, 50, 50);
+  fill(0,255, 0);
+  rect(275, 400, 50, 50 );
+  fill(0,0,255);
+  rect(425,400,50,50);
+  
+  if(mouseX > 125 && mouse X < 175 Mouse Y > 375 && mouseY < 425);
+  {
+  
+}
+}
